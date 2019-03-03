a36a29de5ca9d1ab4df90f3a9a1d8f2774303eee
Luan <luan.b@stjamesperth.com.au>
2019-03-03 11:37:27 +0800

    completed Locationslist component. Have issues with integration testing. Plan to go back to do unit and integration testing after all components completed

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,10 +1,86 @@
 import React, { Component } from 'react';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+import { Icon } from 'element-react';
+
 export class LocationList extends Component {
+  fetchLocationDetails = (e) => {
+    const location = e.target.getAttribute('locationwoeid');
+    console.log('we need to get details for ', location);
+    console.log(e);
+  };
+
+  drawtable = (locations) => {
+    return (
+      <div style={{ overflow: 'auto' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Location</TableCell>
+              <TableCell align="right">Location Type</TableCell>
+              <TableCell align="right">latt_long</TableCell>
+            </TableRow>
+          </TableHead>
+        </Table>
+
+        <div style={{ overflow: 'auto', maxHeight: '250px' }}>
+          <Table style={{ tableLayout: 'fixed' }}>
+            <TableBody>
+              {locations.map((location) => (
+                <TableRow
+                  key={location.title}
+                  locationwoeid={location.woeid}
+                  onClick={() => this.props.locationClicked(location.woeid)}
+                  hover={true}
+                >
+                  <TableCell component="th" scope="row">
+                    {location.title}
+                  </TableCell>
+                  <TableCell align="right"> {location.location_type}</TableCell>
+                  <TableCell align="right"> {location.latt_long}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    );
+  };
+
   render() {
+    const { searchResults } = this.props;
+
+    const locations = searchResults.length > 0 ? searchResults : 0;
+    console.log('location is');
+    console.log(locations);
+
     return (
       <>
-        <div>LocationList</div>
+        {searchResults.length > 0 ? (
+          <h2 className="text-green">
+            <Icon type="success" name="check" className="icon" />
+            {searchResults.length} Results
+          </h2>
+        ) : (
+          <h2 className="header">
+            <img
+              src="https://icon.now.sh/pin_drop/527FFF"
+              alt="Search Location Icon"
+              className="large-icon"
+            />
+            Locations
+          </h2>
+        )}
+        {locations.length > 0 ? (
+          this.drawtable(locations)
+        ) : (
+          <div>No table data yet</div>
+        )}
       </>
     );
   }
--- src/tests/LocationList.test.js
+++ src/tests/LocationList.test.js
@@ -7,22 +7,30 @@ import LocationList from '../components/LocationList';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('LocationList component displays a table or not depending on available data', () => {
-  const dummyData = [{ a: 'foo', b: 'bar' }, { a: 'fooB', c: 'barB' }];
+  const dummyData = [{ a: 'foo', b: 'bar' }];
   const mockHandleLocationClick = jest.fn();
   const mockSearchResultsState = dummyData;
 
-  const wrapper = shallow(
-    <LocationList
-      searchResults={mockSearchResultsState}
-      locationClicked={mockHandleLocationClick}
-    />
-  );
 
   test('component renders', () => {
+    const wrapper = shallow(
+      <LocationList
+
+        locationClicked={mockHandleLocationClick}
+      />
+    );
+
     expect(wrapper.exists()).toBe(true);
   });
-
+/// something wrong with here
   test('component displays table with dummyData', () => {
-    expect(wrapper.find('table').exists()).toBe(true);
+    const wrapper = shallow(
+      <LocationList
+
+        locationClicked={mockHandleLocationClick}
+      />
+    );
+
+    expect(wrapper.prop('searchResults').toExist())).toBe(true);
   });
 });
